393f76cb78efdc086e5127ff7c4fe126ddc0e478
meher12 <meher.khemiri@gmail.com>
2023-06-20 18:01:01 +0100

    Job with Second Tasklet Step

--- 1.0-spring-batch/src/main/java/com/guru2batch/config/SampleJob.java
+++ 1.0-spring-batch/src/main/java/com/guru2batch/config/SampleJob.java
@@ -25,6 +25,7 @@ public class SampleJob {
     public Job firstJob() {
         return jobBuilderFactory.get("First Job")
                 .start(firstStep())
+                .next(secondStep())
                 .build();
     }
 
@@ -39,6 +40,22 @@ public class SampleJob {
             @Override
             public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
                 System.out.println("This is first tasklet step");
+                return RepeatStatus.FINISHED;
+            }
+        };
+    }
+
+    private Step secondStep() {
+        return stepBuilderFactory.get("Second Step")
+                .tasklet(secondTask())
+                .build();
+    }
+
+    private Tasklet secondTask() {
+        return new Tasklet() {
+            @Override
+            public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
+                System.out.println("This is second tasklet step");
                 return RepeatStatus.FINISHED;
             }
         };
--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 ## 1. Getting Started with Spring Batch:
 1. Create First Spring Batch Application
 2. Job with First Tasklet Step
+3. Job with Second Tasklet Step
 
